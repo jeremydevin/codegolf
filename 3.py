6de2e042dc7917a8767a38732121c4a36b3eac17
jeremydevin <jeremy.devin@vanderbilt.edu>
2016-11-29 20:38:10 -0600

    c
    c

---
+++ 3.py
@@ -0,0 +1,9 @@
+a = ord('a')
+b = ord('b')-a
+c = ord('c')-a
+d = ord('d')-a
+f = ord('f')-a
+g = ord('g')-a
+h = ord('h')-a
+print (""+str(b)+" ("+str(g)+str(b)+str(c)+") "+str(d)+str(c)+str(c)+"-"+
+       str(h)+str(d)+str(b)+str(b))
